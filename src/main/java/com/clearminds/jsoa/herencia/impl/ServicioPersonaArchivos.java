3d560080c72791ae816cfa71ff1f8a40e5dec7b0
Juan Sebastian <hammerjuan_@hotmail.com>
2020-12-10 09:34:49 -0500

    Creadas implementaciones e interfaz ServicioPersona

--- src/main/java/com/clearminds/jsoa/herencia/impl/ServicioPersonaArchivos.java
+++ src/main/java/com/clearminds/jsoa/herencia/impl/ServicioPersonaArchivos.java
@@ -3,21 +3,19 @@ package com.clearminds.jsoa.herencia.impl;
 import com.clearminds.jsoa.herencia.interfaces.ServicioPersona;
 import com.clearminds.jsoa.herencia.model.Persona;
 
-public class ServicioPersonaBDD implements ServicioPersona{
-
+public class ServicioPersonaArchivos implements ServicioPersona{
 	public boolean insertar(Persona persona) {
-		System.out.println("Insertando persona en base de datos");
+		System.out.println("Insertando persona en un archivo");
 		return false;
 	}
 
 	public boolean actualizar(Persona persona) {
-		System.out.println("Actualizando persona en base de datos");
+		System.out.println("Actualizando persona en un archivo");
 		return false;
 	}
 
 	public boolean eliminar(Persona persona) {
-		System.out.println("Eliminando persona en base de datos");
+		System.out.println("Eliminando persona en un archivo");
 		return false;
 	}
-	
 }
